2af5037ce7aee308891862bcd732cec9fb6d0e66
Brendon Willian <bmota@its.jnj.com>
2017-07-28 13:25:31 -0300

    - Make auth get from graph api

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const auth = require(`${process.env.PWD}/src/auth`);
+
 const router = express.Router();
 
+const testToken = 'EAAGcDkc4QZC0BAEU7U63XeUivWfDv9CUCIKz7IAcyZCmBylylJfyBZAzjUirYcIp35PIZBca8AQES6KCdyrcZBAkAsAfsICjWZAeZCKy5DTYeQ9U2931Vtxjya9umj9it5dFbcxQXPwxrL1zBlforwRMpdq4GX8U08QR2BisZCCAMEEQhWsoTFycmM2YaZAHpKCsZD';
+
 router.get('/auth', (req, res) => {
-  res.send('Ta mais liso que o batman');
+  auth.setAccessToken(testToken);
+  auth.getCurrentUser().then(user => {
+    res.send(user);
+  });
 });
 
-module.exports = router;
+module.exports = router;  
---
+++ src/auth.js
@@ -0,0 +1,16 @@
+const graph = require('fbgraph');
+
+const setAccessToken = token => {
+    graph.setAccessToken(token);
+}
+
+const getCurrentUser = () => {
+    return new Promise(function (resolve) {
+        graph.get('me?fields=id,name,email,age_range,timezone,locale', (err, res) => resolve(res))
+    });
+}
+
+module.exports = {
+    setAccessToken: setAccessToken,
+    getCurrentUser: getCurrentUser
+}
