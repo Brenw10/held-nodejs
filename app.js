87fc3744cda95fcd12a7c21bab857bd986e41076
Brendon Willian <bmota@its.jnj.com>
2017-07-31 19:48:17 -0300

    - New apis

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const bodyParser = require('body-parser');
 
 const auth = require('./routes/auth');
+const user = require('./routes/user');
 
 const app = express();
 
@@ -10,6 +11,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/api', auth);
+app.use('/api', user);
+
 app.listen(8080);
 
 module.exports = app;
--- routes/user.js
+++ routes/user.js
@@ -5,8 +5,12 @@ const router = express.Router();
 
 const testToken = 'EAAGcDkc4QZC0BALR0orgNzP3smGTrlqKWPPXrNXqaKA6pY3P5PZAgx8aA7VgAAekt724ErZCFpcA8hqRgZAlcJUTPcP5ndW1moPjiBOicMBX6tDhOuQ5LMaBL85fr0Nrsqueq2kz5CY3YIeWHybNVbu5154eGKduVeXNxmOEPdLkoZCA4BSjZAeuk11lqO2l0ZD';
 
-router.get('/auth', (req, res) => {
-  user.isTokenValid(testToken).then(invalid => res.sendStatus(!invalid ? 200 : 401));
+router.get('/user', (req, res) => {
+  user.getUser(testToken).then(user => res.send(user));
+});
+
+router.get('/user/friends', (req, res) => {
+  user.getFriends(testToken).then(friends => res.send(friends));
 });
 
 module.exports = router;  
