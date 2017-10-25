b75ad10296011af06b4c7cc336117af6cb309f03
Brendon Willian <bmota@its.jnj.com>
2017-10-25 16:43:49 -0200

    - posts.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,18 +2,14 @@ const express = require('express');
 
 const router = express.Router();
 
-// App user hierarchy routes
 const user = require('./user');
 const userFriend = require('./user-friend');
 const userPost = require('./user-post');
+const posts = require('./posts');
 
 router.use('/user', user);
 router.use('/user', userFriend);
 router.use('/user', userPost);
-
-// App post hierarchy routes
-const post = require('./post');
-
-router.use('/posts', post);
+router.use('/posts', posts);
 
 module.exports = router;
